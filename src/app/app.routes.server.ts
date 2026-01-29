c40f6b3fccf72a875c50e522340410cb696ddfeb
Pattradanai Punvichartkul <saint12302@gmail.com>
2026-01-30 04:30:34 +0700

    update server render mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { SCREEN_PRINTING_SERIES } from './pages/products/inks/ink-series.model';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/inks/:slug',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () =>
+      SCREEN_PRINTING_SERIES.map(series => ({ slug: series.slug })),
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
